4c3792af75a8974d92f2ec6da79d6ddbc81e3931
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2025-01-22 12:53:22 +0300

    feat: scss for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,15 @@
-import { useState, version } from 'react'
+// import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import './scss/main.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hi from React 19</h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src='../public/vite.svg' className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src='../public/rim.jpg' className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <small>{version}</small>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Продажа шифера по регионам Республики Беларусь.</h1>
+      <h3>Страница в разработке!</h3>
     </>
   )
 }
